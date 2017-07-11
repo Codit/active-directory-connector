fae72f0fc8cd294b148d3ee28450310e115be45c
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-07-11 15:15:41 +0200

    Add basic OWIN API

---
+++ src/Codit.ApiApps.ActiveDirectory/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Codit.ApiApps.ActiveDirectory.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class UsersController : ApiController
+    {
+        [Route("users")]
+        public IHttpActionResult Get()
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ src/Codit.ApiApps.ActiveDirectory/OwinStartup.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using Codit.ApiApps.ActiveDirectory;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinStartup))]
+namespace Codit.ApiApps.ActiveDirectory
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureRoutes(GlobalConfiguration.Configuration);
+            ConfigureFormatters(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configuration.EnsureInitialized();
+        }
+
+        private static void ConfigureRoutes(HttpConfiguration httpConfiguration)
+        {
+            SwaggerConfig.Register();
+            WebApiConfig.Register(httpConfiguration);
+        }
+
+        private static void ConfigureFormatters(HttpConfiguration httpConfiguration)
+        {
+            var jsonFormatter = httpConfiguration.Formatters.JsonFormatter ?? new JsonMediaTypeFormatter();
+
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            jsonFormatter.SerializerSettings.Formatting = Formatting.None;
+            jsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
+
+            httpConfiguration.Formatters.Remove(httpConfiguration.Formatters.JsonFormatter);
+            httpConfiguration.Formatters.Insert(0, jsonFormatter);
+        }
+    }
+}
