54fabe28519dc31b58a7af0a612a1eee5e3af70d
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-07-12 19:35:01 +0200

    Remove temp code

--- src/Codit.ApiApps.ActiveDirectory/Controllers/UsersController.cs
+++ src/Codit.ApiApps.ActiveDirectory/Controllers/UsersController.cs
@@ -12,38 +12,7 @@ namespace Codit.ApiApps.ActiveDirectory.Controllers
         [Route("users")]
         public async Task<IHttpActionResult> Get()
         {
-            var user = await GetAllUsers();
-            return Ok(user);
-        }
-
-        private async Task<string> GetAllUsers()
-        {
-            ActiveDirectoryClient activeDirectoryClient = new ActiveDirectoryClient(serviceUri, GetAccessToken);
-            var users = activeDirectoryClient.Users.Take(10);
-            return "John Doe";
-        }
-
-        private static string clientId = "72a2b371-ad5f-4ce7-a688-e132adeb6260";
-        private static string appKey = "GdlG1uyl2BgBs+DPgHrzD7/hZUe9ccAL0sCmHMtn+pQ=";
-        private static readonly string rawServiceUri = "https://graph.windows.net/codit.onmicrosoft.com";
-        private static readonly Uri serviceUri = new Uri(rawServiceUri);
-        private static string authority = "https://login.microsoftonline.com/codit.onmicrosoft.com";
-
-        private static async Task<string> GetAccessToken()
-        {
-            string accessToken = await GetAppToken(authority, "https://graph.windows.net");
-
-            return accessToken;
-        }
-
-        public static async Task<string> GetAppToken(string authority, string resource)
-        {
-            var authenticationContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
-            var clientCredential = new ClientCredential(clientId, appKey);
-
-            var token = await authenticationContext.AcquireTokenAsync(resource, clientCredential);
-
-            return token.AccessToken;
+            return Ok();
         }
     }
 }
