5dafad3e8df6675ca4036fce586a465145b7c65a
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-07-14 00:44:18 +0200

    Provide documentation in Swagger

--- src/Codit.ApiApps.ActiveDirectory/Controllers/UsersController.cs
+++ src/Codit.ApiApps.ActiveDirectory/Controllers/UsersController.cs
@@ -1,25 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Codit.ApiApps.ActiveDirectory.Repositories;
+using Swashbuckle.Swagger.Annotations;
 
 namespace Codit.ApiApps.ActiveDirectory.Controllers
 {
     [RoutePrefix("api/v1")]
     public class UsersController : ApiController
     {
-        private readonly UserRepository userRepository = new UserRepository();
+        private readonly UserRepository _userRepository = new UserRepository();
 
+        /// <summary>
+        ///     Gets all users in Active Directory
+        /// </summary>
         [Route("users")]
+        [SwaggerResponse(HttpStatusCode.OK, "Returns all users", typeof(List<string>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, "No users were found")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, "We were unable to successfully process the request")]
         public async Task<IHttpActionResult> GetUsers()
         {
-            var users = await userRepository.Get();
-            return Ok(users);
+            var users = await _userRepository.Get();
+
+            return users.Any() ? (IHttpActionResult)Ok(users) : NotFound();
         }
 
+        /// <summary>
+        ///     Gets a specific user
+        /// </summary>
+        /// <param name="objectId">Object Id for the user to lookup</param>
         [Route("user")]
+        [SwaggerResponse(HttpStatusCode.OK, "Returns found user", typeof(string))]
+        [SwaggerResponse(HttpStatusCode.NotFound, "User with specified objectId was not found")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, "We were unable to successfully process the request")]
         public async Task<IHttpActionResult> GetUser(string objectId)
         {
-            var user = await userRepository.Get(objectId);
+            var user = await _userRepository.Get(objectId);
             return Ok(user);
         }
     }
