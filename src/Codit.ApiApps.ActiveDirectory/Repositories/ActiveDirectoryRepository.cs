92c165129c9dbc82240f81ac426f34a5bc860c0f
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-07-14 17:30:54 +0200

    Introduce ActiveDirectoryRepository

---
+++ src/Codit.ApiApps.ActiveDirectory/Repositories/ActiveDirectoryRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Codit.ApiApps.ActiveDirectory.Repositories
+{
+    public class ActiveDirectoryRepository
+    {
+        private static readonly string ServiceUriTemplate = "https://graph.windows.net/{0}";
+        private static readonly string AuthorityTemplate = "https://login.microsoftonline.com/{0}";
+
+        protected ActiveDirectoryClient GetActiveDirectoryClient()
+        {
+            var serviceUri = GetServiceUri();
+            var activeDirectoryClient = new ActiveDirectoryClient(serviceUri, GetAccessToken);
+            return activeDirectoryClient;
+        }
+
+        private static Uri GetServiceUri()
+        {
+            var activeDirectoryTenant = GetActiveDirectoryTenant();
+            var rawServiceUri = string.Format(ServiceUriTemplate, activeDirectoryTenant);
+            return new Uri(rawServiceUri);
+        }
+
+        private static string GetActiveDirectoryTenant()
+        {
+            var activeDirectoryTenant = ConfigurationManager.AppSettings.Get("ActiveDirectory.Tenant");
+            return activeDirectoryTenant;
+        }
+
+        private static async Task<string> GetAccessToken()
+        {
+            var activeDirectoryTenant = GetActiveDirectoryTenant();
+            var authority = string.Format(AuthorityTemplate, activeDirectoryTenant);
+            var accessToken = await GetAppToken(authority, "https://graph.windows.net");
+
+            return accessToken;
+        }
+
+        private static string GetClientId()
+        {
+            var clientId = ConfigurationManager.AppSettings.Get("ActiveDirectory.QueryApplication.ClientId");
+            return clientId;
+        }
+
+        public static async Task<string> GetAppToken(string authority, string resource)
+        {
+            var clientId = GetClientId();
+            var appKey = GetAppKey();
+            var authenticationContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var clientCredential = new ClientCredential(clientId, appKey);
+
+            var token = await authenticationContext.AcquireTokenAsync(resource, clientCredential);
+
+            return token.AccessToken;
+        }
+
+        private static string GetAppKey()
+        {
+            // TODO: Use Key Vault instead
+            var appKey = ConfigurationManager.AppSettings.Get("ActiveDirectory.QueryApplication.AppKey");
+            return appKey;
+        }
+    }
+}
--- src/Codit.ApiApps.ActiveDirectory/Repositories/UserRepository.cs
+++ src/Codit.ApiApps.ActiveDirectory/Repositories/UserRepository.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.ActiveDirectory.GraphClient;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using User = Codit.ApiApps.ActiveDirectory.Contracts.v1.User;
 
 namespace Codit.ApiApps.ActiveDirectory.Repositories
 {
-    public class UserRepository
+    public class UserRepository: ActiveDirectoryRepository
     {
-        private static readonly string ServiceUriTemplate = "https://graph.windows.net/{0}";
-        private static readonly string AuthorityTemplate = "https://login.microsoftonline.com/{0}";
-
         /// <summary>
         ///     Gets a specific user
         /// </summary>
@@ -62,60 +56,6 @@ namespace Codit.ApiApps.ActiveDirectory.Repositories
             };
 
             return user;
-        }
-
-        private static ActiveDirectoryClient GetActiveDirectoryClient()
-        {
-            var serviceUri = GetServiceUri();
-            var activeDirectoryClient = new ActiveDirectoryClient(serviceUri, GetAccessToken);
-            return activeDirectoryClient;
-        }
-
-        private static Uri GetServiceUri()
-        {
-            var activeDirectoryTenant = GetActiveDirectoryTenant();
-            var rawServiceUri = string.Format(ServiceUriTemplate, activeDirectoryTenant);
-            return new Uri(rawServiceUri);
-        }
-
-        private static string GetActiveDirectoryTenant()
-        {
-            var activeDirectoryTenant = ConfigurationManager.AppSettings.Get("ActiveDirectory.Tenant");
-            return activeDirectoryTenant;
-        }
-
-        private static async Task<string> GetAccessToken()
-        {
-            var activeDirectoryTenant = GetActiveDirectoryTenant();
-            var authority = string.Format(AuthorityTemplate, activeDirectoryTenant);
-            var accessToken = await GetAppToken(authority, "https://graph.windows.net");
-
-            return accessToken;
-        }
-
-        private static string GetClientId()
-        {
-            var clientId = ConfigurationManager.AppSettings.Get("ActiveDirectory.QueryApplication.ClientId");
-            return clientId;
-        }
-
-        public static async Task<string> GetAppToken(string authority, string resource)
-        {
-            var clientId = GetClientId();
-            var appKey = GetAppKey();
-            var authenticationContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
-            var clientCredential = new ClientCredential(clientId, appKey);
-
-            var token = await authenticationContext.AcquireTokenAsync(resource, clientCredential);
-
-            return token.AccessToken;
-        }
-
-        private static string GetAppKey()
-        {
-            // TODO: Use Key Vault instead
-            var appKey = ConfigurationManager.AppSettings.Get("ActiveDirectory.QueryApplication.AppKey");
-            return appKey;
         }
     }
 }
